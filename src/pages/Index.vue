33077805e332b5947d1e5baeb78b84b13281fd84
Adam Whitlock <awhitlock@dealerinspire.com>
2020-08-01 23:23:08 -0600

    add copy button item styles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <div v-if="stream.length > 0">
-      <div class="row">
-        <div class="col q-pa-sm">Total entries: {{ stream.length }}</div>
+  <q-page expand class="flex ">
+    <div v-if="stream.length > 0" class="full-width">
+      <div class="row justify-between items-center q-pa-sm bg-cyan-1">
+        <div class="col">Total entries: {{ stream.length }}</div>
+        <div class="col justify-end flex">
+          <q-btn
+            outline
+            rounded
+            color="primary"
+            @click.prevent="clearClipStream"
+            label="Clear Entries"
+            icon="delete_forever"
+          />
+        </div>
       </div>
       <div
         v-for="item in stream"
         :key="item.id"
-        class="row flex items-center q-pa-md inset-shadow"
+        class="row flex items-center q-pa-md shadow-2 rounded-borders q-mt-md q-mx-sm entry-wrapper"
       >
-        <div class="col-10 flex items-center">
-          <p class="q-ma-none">
-            {{ item.text }} | {{ item.charCount }} Chars |
-            {{ formatDate(item.createdAt) }}
+        <div class="col-auto">
+          <q-btn
+            round
+            flat
+            color="blue-grey-4"
+            @click.prevent="copyClip(item.text)"
+            icon="content_copy"
+            class="entry-copy"
+          />
+        </div>
+        <div class="col items-center justify-start">
+          <p class="q-ma-none text-blue-grey-7">
+            {{ formatDate(item.createdAt) }} | {{ item.charCount }} Chars
           </p>
+          <pre class="q-ma-none">{{ item.text }}</pre>
         </div>
 
-        <div class="col-2 flex justify-end">
+        <div class="col-auto flex justify-end">
           <q-btn flat round color="red" icon="favorite" />
           <q-btn flat round color="teal" icon="bookmark" />
           <q-btn flat round color="primary" icon="share" />
@@ -37,6 +57,19 @@ export default {
     };
   },
   methods: {
+    async clearClipStream() {
+      try {
+        const removed = await this.$db.stream.remove({}, { multi: true });
+        console.log({ removed });
+        this.stream = [];
+      } catch (err) {
+        console.log("ERROR LOADING CLIP STREAM");
+        console.log(err);
+      }
+    },
+    copyClip(clip) {
+      console.log({ clip });
+    },
     async getClipStream() {
       try {
         this.stream = await this.$db.stream.find({}).sort({ createdAt: -1 });
@@ -57,3 +90,13 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.entry-copy {
+  opacity: 0;
+}
+
+.entry-wrapper:hover .entry-copy {
+  opacity: 1;
+}
+</style>
